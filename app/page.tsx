34ea849fa63110fdff400aece9b68fb193dc47ed
Vladislav Umarov <vladdevs@yandex.ru>
2023-09-03 08:00:52 +0300

    fix: small fix

--- app/page.tsx
+++ app/page.tsx
@@ -33,9 +33,9 @@ export default async function IndexPage() {
 
               <Link href={`/post/${post.slug.current}`} prefetch className='space-y-3 xl:col-span-3'>
                 <div>
-                  <h3 className='text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100'>
+                  <h2 className='text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100'>
                     {post.title}
-                  </h3>
+                  </h2>
                 </div>
 
                 <p className='prose max-w-none text-gray-500 dark:text-gray-400 line-clamp-2'>{post.overview}</p>
